1e6c6e93d29f2a3163b633cba6874ce346123c5c
Zach-Carter <64286899+Zach-Carter@users.noreply.github.com>
2021-02-20 18:48:19 -0800

    Add files via upload

---
+++ lesson7/js/windchill.js
@@ -0,0 +1,12 @@
+function windchill{
+var windsp= 5;
+var temp= 76;
+var windchil="N/A";
+if(temp<= 10 && windsp>= 5){
+var windchil= 35.74 + 0.6215*tempt - 35.75 * math.pow(windsp,0.16) + 0.4275*temp*(windsp,0.16);
+}
+else{
+var windchil= "N/A";
+}
+document.write("windchil");
+}
